1196af88c291f061002c08257f172bd8074464a6
josemanuel.rodriguez.lorenzo@iesdonana.org <manuel@vbox.Home>
2024-12-11 12:19:58 +0100

    biblio al fin

---
+++ biblioteca/app/Models/Ejemplar.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ejemplar extends Model
+{
+    /** @use HasFactory<\Database\Factories\EjemplarFactory> */
+    use HasFactory;
+
+    protected $table = 'ejemplares';
+
+    public function libro()
+    {
+        return $this->belongsTo(Libro::class);
+    }
+
+    public function prestamos()
+    {
+        return $this->hasMany(Prestamo::class);
+    }
+}
---
+++ biblioteca/database/migrations/2024_12_09_151457_create_prestamos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ejemplar_id')->constrained('ejemplares')->onDelete('cascade');
+            $table->foreignId('cliente_id')->constrained()->onDelete('cascade');
+            $table->timestamp('fecha_hora')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prestamos');
+    }
+};
